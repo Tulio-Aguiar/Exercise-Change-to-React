2d439e770716e4ac613a76a5981cf3f12b474f9b
Túlio Aguiar <axeaguiar@hotmail.com>
2026-03-26 09:44:08 +0100

    chore: init from Code-And-Safety template

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-green-500">Tailwind v4 funcionando</h1>
+      {' '}
+      <div className="container mx-auto max-w-7xl">
+        <h1 className="text-3xl font-bold text-green-500">Projeto Code and Safety</h1>
+        <p className="text-lg text-gray-500">
+          Este é um projeto de exemplo para demonstrar o uso do Tailwind CSS v4.
+        </p>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-8">Clique aqui</button>
+      </div>
     </>
   )
 }
